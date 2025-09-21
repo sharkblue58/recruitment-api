66f8224293e104043ef68e9f00e23f73d7415f83
Mohamed Essam <zoroloffy95@gmail.com>
2025-09-21 17:51:06 +0200

    add global exception handler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,103 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Spatie\Permission\Exceptions\UnauthorizedException;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
-    })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    })->withExceptions(function (Exceptions $exceptions): void {
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => "resource not found",
+                    'status' => 404
+                ], 404);
+            }
+        });
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => "you are unauthorized !",
+                    'status' => 401
+                ], 401);
+            }
+        });
+        $exceptions->render(function (UnauthorizedException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => "you are forbidden !",
+                    'status' => 403
+                ], 403);
+            }
+        });
+        $exceptions->render(function (MethodNotAllowedHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => "method not allowed !",
+                    'status' => 405
+                ], 405);
+            }
+        });
+
+        $exceptions->render(function (ValidationException $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => "validation error",
+                    'errors' => $e->errors(),
+                    'status' => 422
+                ], 422);
+            }
+        });
+        $exceptions->render(function (ThrottleRequestsException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => "you sent too many requests",
+                    'status' => 429
+                ], 429);
+            }
+        });
+        $exceptions->render(function (HttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => "server in maintenance now",
+                    'status' => 503
+                ], 503);
+            }
+        });
+        $exceptions->render(function (Throwable $e, Request $request) {
+            if ($request->is('api/*')) {
+                logger([
+                    'message' => $e->getMessage(),
+                    'exception' => get_class($e),
+                ]);
+                return response()->json([
+                    'success' => false,
+                    'message' => "internal server error",
+                    'status' => 500
+                ], 500);
+            }
+        });
     })->create();
